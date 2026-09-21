e9f1c5a68ab604b6b0ff302454ee17ee86507a88
aoisora-design <sachima369@gmail.com>
2026-09-21 13:58:42 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 
 const menuButton = document.querySelector('.sp-header-btn-link');
 const spMenu = document.querySelector('.sp-menu');
+const headerBtn = document.querySelector('.sp-header-btn');
 
 menuButton.addEventListener('click', function() {
     spMenu.classList.toggle('is-open');
+    headerBtn.classList.toggle('is-open');
 });
